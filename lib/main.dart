a3c8c8626b4952034fd52a195495855762e331b5
Richard nwonah <nwonahr@gmail.com>
2021-09-07 18:50:22 +0100

    Added dark screen project

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,51 @@
+import 'Package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Color(0xff009688),
+        accentColor: Color(0xff7C4DFF),
+        textTheme: TextTheme(
+            bodyText2: TextStyle(
+          fontSize: 24,
+          fontStyle: FontStyle.italic,
+        )),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Building layouts with flutter'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello Flutter Layouts',
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.lightbulb_outline),
+            onPressed: () {
+              print('You rang?');
+            }),
+        persistentFooterButtons: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add_comment),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_alarm_sharp),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_location),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
